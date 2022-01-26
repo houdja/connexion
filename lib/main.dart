ddfb6028ce319e17380075d93fc79671ff5b0c13
houdja test <houdja-abdallahpro@hotmail.com>
2022-01-26 19:43:47 +0400

    Ajout d'un police + style page connexion

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -10,8 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Maven Pro',
+        scaffoldBackgroundColor: Colors.orangeAccent,
+      ),
       home: Register(),
     );
   }
@@ -29,52 +34,189 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40),
+        padding: EdgeInsets.symmetric(horizontal: 60),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            Text('LOGO'),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Connectez vous à votre compte'.toUpperCase(),
-                  style: TextStyle(fontSize: 22),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Connectez vous à votre compte'.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 Myrow(FontAwesomeIcons.user, 'Jhon'),
                 Myrow(FontAwesomeIcons.eye, 'Mot de passe'),
               ],
             ),
             Container(
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 0,
+                    blurRadius: 10,
+                    offset: Offset(0, 5), // changes position of shadow
+                  ),
+                ],
+              ),
               width: double.infinity,
               child: TextButton(
                 onPressed: () {},
-                child: Text(
-                  'Se connecter'.toUpperCase(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Text(
+                    'connexion'.toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 5,
+                      fontSize: 17,
+                    ),
+                  ),
                 ),
               ),
             ),
             Column(
               children: [
-                Text(
-                  'Connectez vous avec'.toUpperCase(),
+                Container(
+                  margin: EdgeInsets.only(bottom: 40),
+                  child: Text(
+                    'Connectez vous avec'.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const Icon(FontAwesomeIcons.facebook),
-                    const Icon(FontAwesomeIcons.instagram),
-                    const Icon(FontAwesomeIcons.google),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 10,
+                            offset: Offset(0, 5), // changes position of shadow
+                          )
+                        ],
+                      ),
+                      padding: EdgeInsets.all(12.5),
+                      child: Icon(
+                        FontAwesomeIcons.google,
+                        color: Colors.black,
+                        size: 22,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 10,
+                            offset: Offset(0, 5), // changes position of shadow
+                          )
+                        ],
+                      ),
+                      padding: EdgeInsets.all(12.5),
+                      child: Icon(
+                        FontAwesomeIcons.facebook,
+                        color: Colors.black,
+                        size: 22,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 10,
+                            offset: Offset(0, 5), // changes position of shadow
+                          )
+                        ],
+                      ),
+                      padding: EdgeInsets.all(12.5),
+                      child: Icon(
+                        FontAwesomeIcons.instagram,
+                        size: 22,
+                        color: Colors.black,
+                      ),
+                    ),
                   ],
                 )
               ],
             ),
             Column(
               children: [
-                Text(
-                  'Mot de passe oublié'.toUpperCase(),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0xfff1f1f1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 0,
+                        blurRadius: 10,
+                        offset: Offset(0, 5), // changes position of shadow
+                      )
+                    ],
+                  ),
+                  width: double.infinity,
+                  margin: EdgeInsets.only(bottom: 15),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        'Mot de passe oublié'.toUpperCase(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
                 ),
-                Text(
-                  'S\'inscrire'.toUpperCase(),
-                )
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xfff1f1f1),
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 0,
+                        blurRadius: 10,
+                        offset: Offset(0, 5), // changes position of shadow
+                      )
+                    ],
+                  ),
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "S'inscrire".toUpperCase(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             )
           ],
@@ -93,19 +235,26 @@ class Myrow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: EdgeInsets.symmetric(vertical: 15),
+      margin: EdgeInsets.only(top: 30),
       decoration: BoxDecoration(
-        border: Border.all(
-          width: 0.5,
-          color: Colors.deepPurple,
-        ),
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 0,
+            blurRadius: 10,
+            offset: Offset(0, 5), // changes position of shadow
+          )
+        ],
+        color: Color(0xfff1f1f1),
       ),
       child: Row(
         children: [
           Padding(
-            padding: EdgeInsets.fromLTRB(12, 12, 30, 12),
+            padding: EdgeInsets.fromLTRB(30, 15, 40, 15),
             child: Icon(
               i,
+              size: 20,
             ),
           ),
           Text(
