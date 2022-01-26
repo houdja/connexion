44fe25a817cd8e11a1a5d80af7976c687aaa748e
houdja test <houdja-abdallahpro@hotmail.com>
2022-01-26 20:56:52 +0400

    Creation de la page motdepasse.dart

---
+++ lib/pages/motdepasse.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../Myrow.dart';
+import '../Mybutton.dart';
+import '../main.dart';
+
+class Password extends StatelessWidget {
+  const Password({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Accueil'),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Text(
+                'Mot de passe oublié'.toUpperCase(),
+                style: TextStyle(fontSize: 22),
+              ),
+            ),
+            Myrow(FontAwesomeIcons.at, "Entrez votre email"),
+            Mybutton("Reinitialiser le mot de passe", 30, Register())
+          ],
+        ),
+      ),
+    );
+  }
+}
