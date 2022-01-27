b7bbcccb179517983ce3f32f83ba42dc93afed57
houdja test <houdja-abdallahpro@hotmail.com>
2022-01-27 14:38:17 +0400

    Test recup valeur

--- lib/pages/inscription.dart
+++ lib/pages/inscription.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../Myrow.dart';
-import '../Mybutton.dart';
+
+TextEditingController username = TextEditingController();
+TextEditingController password = TextEditingController();
+TextEditingController passwordBis = TextEditingController();
+TextEditingController mail = TextEditingController();
 
 class Inscription extends StatelessWidget {
-  test() {
-    print('eeee');
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  register() {
+    String nameUser = username.text;
+    String passwordUser = password.text;
+    String passwordBisUser = passwordBis.text;
+    String mailUser = mail.text;
+    print(
+        nameUser + " " + passwordUser + " " + passwordBisUser + " " + mailUser);
   }
 
   @override
@@ -21,21 +32,53 @@ class Inscription extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              child: Text(
-                'Inscription'.toUpperCase(),
-                style: TextStyle(fontSize: 22),
+            Form(
+              key: _formKey,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 20),
+                child: Text(
+                  'Inscription'.toUpperCase(),
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-            Myrow("Nom d'utilisateur", FontAwesomeIcons.user),
-            Myrow("Mot de passe", FontAwesomeIcons.key),
-            Myrow("Retapper mot de passe", FontAwesomeIcons.key),
-            Myrow("Email", FontAwesomeIcons.at),
-            Mybutton(
-              "S'enregistrer",
-              30,
-            )
+            Myrow("Nom d'utilisateur", FontAwesomeIcons.user, username),
+            Myrow("Mot de passe", FontAwesomeIcons.key, password),
+            Myrow("Retapper mot de passe", FontAwesomeIcons.key, passwordBis),
+            Myrow("Email", FontAwesomeIcons.at, mail),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
+                borderRadius: BorderRadius.circular(0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 0,
+                    blurRadius: 10,
+                    offset: Offset(0, 5), // changes position of shadow
+                  )
+                ],
+              ),
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    register();
+                  }
+                },
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    'Inscription'.toUpperCase(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
